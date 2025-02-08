9ea0c2aad84e790d8e31fcbc598bea2370f5a948
mohamed12345abdullah <112867836+mohamed12345abdullah@users.noreply.github.com>
2025-02-08 19:13:18 +0200

    push

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use('/',express.static('public/'))
 // Swagger Configuration
 const swaggerOptions = {
   definition: {
